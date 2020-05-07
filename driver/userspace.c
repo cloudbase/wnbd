aserdean/wnbd-1

driver: Export WnbdDeleteConnection signature
This patch exports the WnbdDeleteConnection to its corresponding header.

Signed-off-by: Alin Gabriel Serdean <aserdean@cloudbasesolutions.com>

driver/userspace.c
@@ -388,9 +388,10 @@ WnbdSetDeviceMissing(_In_ PVOID Handle,
     return TRUE;
 }
 
+_Use_decl_annotations_
 NTSTATUS
-WnbdDeleteConnection(_In_ PGLOBAL_INFORMATION GInfo,
-                     _In_ PUSER_IN Info)
+WnbdDeleteConnection(PGLOBAL_INFORMATION GInfo,
+                     PUSER_IN Info)
 {
     WNBD_LOG_LOUD(": Enter");
     ASSERT(GInfo);

driver/userspace.h
@@ -66,6 +66,10 @@ NTSTATUS
 WnbdEnumerateActiveConnections(_In_ PGLOBAL_INFORMATION GInfo,
                                _In_ PIRP Irp);
 
+NTSTATUS
+WnbdDeleteConnection(_In_ PGLOBAL_INFORMATION GInfo,
+                     _In_ PUSER_IN Info);
+
 VOID
 WnbdInitScsiIds();
 

cloudbase/wnbd

driver/userspace.c
@@ -388,9 +388,10 @@ WnbdSetDeviceMissing(_In_ PVOID Handle,
     return TRUE;
 }
 
+_Use_decl_annotations_
 NTSTATUS
-WnbdDeleteConnection(_In_ PGLOBAL_INFORMATION GInfo,
-                     _In_ PCONNECTION_INFO Info)
+WnbdDeleteConnection(PGLOBAL_INFORMATION GInfo,
+                     PCONNECTION_INFO Info)
 {
     WNBD_LOG_LOUD(": Enter");
     ASSERT(GInfo);

driver/userspace.h
@@ -66,6 +66,10 @@ NTSTATUS
 WnbdEnumerateActiveConnections(_In_ PGLOBAL_INFORMATION GInfo,
                                _In_ PIRP Irp);
 
+NTSTATUS
+WnbdDeleteConnection(_In_ PGLOBAL_INFORMATION GInfo,
+                     _In_ PCONNECTION_INFO Info);
+
 VOID
 WnbdInitScsiIds();
 