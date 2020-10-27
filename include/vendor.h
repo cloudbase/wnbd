3625f47a62817e052d2c69a405cb36f9e01433da
Mike Latimer <mlatimer@suse.com>
2020-10-27 15:31:48 -0600

    Add vendor.h for vendor specific properties
    Modify wnbd.rc to include a vendor.h file where vendor specific strings
    can be maintained. Vendors can modify this file or overwrite it with
    their own data at build time.
    
    Signed-off-by: Mike Latimer <mlatimer@suse.com>

---
+++ include/vendor.h
@@ -0,0 +1,10 @@
+/*-
+ * The variables in this vendor.h file can be used to provide vendor specific
+ * strings to the properities of the WNBD driver. Replace the contents of
+ * this file as necessary.
+ */
+
+#define WNBD_COMPANY_STR "WNBD"
+#define WNBD_FILEDESCRIPTION_STR "WNBD"
+#define WNBD_PRODUCTNAME_STR "WNBD"
+
