3e3ddfca876c5459432891dd228acd14bbce5368
Alin Gabriel Serdean <aserdean@cloudbasesolutions.com>
2020-11-04 12:52:11 +0200

    Switch from devcon to wnbd-client in our reinstall script
    Signed-off-by: Alin Gabriel Serdean <aserdean@cloudbasesolutions.com>

--- vstudio/reinstall.ps1
+++ vstudio/reinstall.ps1
@@ -1,21 +1,18 @@
 $scriptLocation = [System.IO.Path]::GetDirectoryName(
     $myInvocation.MyCommand.Definition)
 
-$devconBin = "$scriptLocation\devcon.exe"
+$wnbdBin = "$scriptLocation\wnbd-client.exe"
 $wnbdInf = "$scriptLocation\wnbd.inf"
 $wnbdCat = "$scriptLocation\wnbd.cat"
 $wnbdSys = "$scriptLocation\wnbd.sys"
 
-$requiredFiles = @($devconBin, $wnbdInf, $wnbdCat, $wnbdSys)
+$requiredFiles = @($wnbdBin, $wnbdInf, $wnbdCat, $wnbdSys)
 foreach ($path in $requiredFiles) {
     if (!(Test-Path -Path $path -PathType leaf)) {
         Write-Warning "Could not find file: $path"
     }
 }
 
-& $devconBin remove "root\wnbd"
+& $wnbdBin uninstall-driver
 
-pnputil.exe /enum-drivers | sls -Context 5 wnbd | findstr Published | `
-    % {$_ -match "(oem\d+.inf)"; pnputil.exe /delete-driver $matches[0] /force }
-
-& $devconBin install $wnbdInf root\wnbd
+& $wnbdBin install-driver $wnbdInf
