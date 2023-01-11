f6c98dad8614c26d7844089b0d8ab4a555c0bf9b
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2023-01-11 05:07:03 -0800

    driver: fix device addressing
    We're using a bitmap in order to determine available SCSI addresses.
    The issue is that we aren't correctly allocating the bitmap, which
    means that we can exceed the maximum number of advertised buses.
    
    This change ensures that we properly handle SCSI addresses, even
    if the maximum number of luns per target changes.
    
    Note that if we do decide to use multiple luns per target, we'll
    need to update WnbdReportLuns as well.
    
    Signed-off-by: Lucian Petrut <lpetrut@cloudbasesolutions.com>

--- driver/driver.h
+++ driver/driver.h
@@ -9,9 +9,15 @@
 
 #include "version.h"
 
+// Maximum number of scsi targets per bus
 #define MAX_NUMBER_OF_SCSI_TARGETS       128
+// Maximum number of luns per target
 #define MAX_NUMBER_OF_SCSI_LOGICAL_UNITS 1
 #define MAX_NUMBER_OF_SCSI_BUSES         1
+// The maximum number of disks per WNBD adapter
+#define MAX_NUMBER_OF_DISKS (MAX_NUMBER_OF_SCSI_LOGICAL_UNITS * \
+                             MAX_NUMBER_OF_SCSI_TARGETS * \
+                             MAX_NUMBER_OF_SCSI_BUSES)
 
 #define WNBD_INQUIRY_VENDOR_ID           "WNBD"
 #define WNBD_INQUIRY_PRODUCT_ID          "WNBD_DISK"
